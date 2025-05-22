c1ca8dbc50472db0bef0ccb5a99e72e329f6b7ec
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2025-05-22 15:15:05 -0300

    implementar formatação de data e hora

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ botaoAdicionar.addEventListener('click', (evento) => {
         tagLista.appendChild(tagContainer);
         listaDeCompras.appendChild(tagLista);
 
+        const diaDaSemana = new Date().toLocaleDateString('pt-BR', {weekday: 'long'});
+        const data = new Date().toLocaleDateString('pt-BR');
+        const hora = new Date().toLocaleTimeString('pt-BR', {hour: 'numeric', minute: 'numeric'});
+        let dataCompleta = `${diaDaSemana} (${data}) ás ${hora}`;
 })
